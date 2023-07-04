f54016cf06902515fca079d8cc2300ceceefed08
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-07-04 03:24:21 -0400

    add google verify file (#16)
    * add google verify file
    
    * move to _static directory
    
    * set html_static_path
    
    * fix
    
    * setup
    
    * setup
    
    * fix return
    
    * fix

--- conf.py
+++ conf.py
@@ -1,4 +1,8 @@
 from datetime import date
+from pathlib import Path
+
+from sphinx.util.fileutil import copy_asset_file
+
 
 project = 'DeepModeling'
 copyright = '2021-%d, DeepModeling' % date.today().year
@@ -8,3 +12,16 @@ extensions = [
     'deepmodeling_sphinx',
 ]
 html_theme = 'sphinx_rtd_theme'
+
+def copy_custom_files(app):
+    if app.builder.format == "html":
+        staticdir = os.path.join(app.builder.outdir)
+        cwd = Path(__file__).parent.absolute()
+        google_html = cwd / "googleb1dcd004739ab7d2.html"
+        os.makedirs(staticdir, exist_ok=True)
+        copy_asset_file(str(google_html), staticdir)
+
+
+def setup(app):
+    app.connect("builder-inited", copy_custom_files)
+    return {"parallel_read_safe": True}
